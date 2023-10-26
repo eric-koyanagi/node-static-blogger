39aa343d6cde06db008e8a7a4f48c14b4c71bda5
eric koyanagi <eric.koyanagi@thetot.com>
2023-10-26 12:52:24 -0700

    Refine index view for preview and edit links

--- routes/index.js
+++ routes/index.js
@@ -18,14 +18,14 @@ router.get("/", article_controller.article_list);
 router.get("/article/create", article_controller.article_create_get);
 router.post("/article/create", article_controller.article_create_post);
 
-// POST for article update
-//router.get("/book/:id/update", article_controller.article_create_post);
+// GET for article update
+router.get("/article/edit/:id", article_controller.article_detail);
 
 // POST request to delete
 router.post("/article/:id/delete", article_controller.article_delete_post);
 
 // GET request to render an article for preview
-router.get("/article/edit/:id", article_controller.article_detail);
+router.get("/article/view/:id", article_controller.article_detail);
 
 // TODO: add other controller routes here, or separate them into different files if this becomes too lengthy
 
