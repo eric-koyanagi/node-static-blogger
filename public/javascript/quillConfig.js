f80b16267eca8c0ce8ea648bcf24442a97d9ee4e
eric koyanagi <eric.koyanagi@thetot.com>
2023-11-01 13:50:07 -0700

    Remove bootstrap from admin, rework with Tailwind instead

--- public/javascript/quillConfig.js
+++ public/javascript/quillConfig.js
@@ -1,3 +1,7 @@
+/*
+* This configures Quill with all desired toolbar options
+* We extend some Quill blocks so we can utilize Tailwind and consistently format rendered pages without any CSS bloat or inline styles
+*/
 var toolbarOptions = [
   ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
   ['blockquote', 'code-block'],
@@ -18,7 +22,17 @@ var toolbarOptions = [
   ['clean']                                         // remove formatting button
 ];
 
-// Extend quill formats here to integrate with tailwind
+// Extend quill formats to inject custom classes and styling via TailWind; Tailwind will detect classes added here
+var Block = Quill.import('blots/block');
+class CustomBlock extends Block {
+  static create() {
+    const node = super.create();
+    node.classList.add('mb-2');
+    node.classList.add('text-gray-900');
+    return node;
+  }
+}
+
 var CodeBlock = Quill.import('formats/code-block'); 
 class CustomCodeBlock extends CodeBlock {
   static create() {
@@ -34,9 +48,32 @@ class CustomCodeBlock extends CodeBlock {
   }
 }
 
-Quill.register(CustomCodeBlock, true);
+var Header = Quill.import('formats/header'); 
+class CustomHeader extends Header {
+  static create(headingLevel) {
+    const node = super.create(headingLevel);    
+    const headingClasses = {
+      1: 'text-2xl',
+      2: 'text-xl',
+      3: 'text-lg',
+      4: 'text-lg',
+      5: 'text-base',
+      6: 'text-base'
+    };
 
-// Initializes quill
+    node.classList.add(headingClasses[headingLevel]);
+    node.classList.add('text-cyan-600');
+
+    return node;
+  }
+}
+
+// Register our custom blocks
+Quill.register(CustomBlock, true);
+Quill.register(CustomCodeBlock, true);
+Quill.register(CustomHeader, true);
+
+// Initialize actual widget
 var quill = new Quill('#articleBody', {
   theme: 'snow',
   modules: {
@@ -44,8 +81,8 @@ var quill = new Quill('#articleBody', {
   }
 });
 
-// adds quill-based content to actual form, since it exists in a div 
-function updateBody(ev) {    
+// adds HTML to the form, since it exists only in a div
+function updateBody(ev) {
   document.getElementById("body").value = quill.root.innerHTML;
 }
 
